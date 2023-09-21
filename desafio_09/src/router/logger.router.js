b8c80450337c82649eae3c379224e9702b38b2c6
Julian Arbeo Mateo <j.arbeo@gmail.com>
2023-09-21 20:51:10 -0300

    Delete desafio_09 directory

--- desafio_09/src/router/logger.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loggerTestEndpoint } from "../controllers/loggerTest.js";
-
-const router = express.Router();
-
-router.get("/", loggerTestEndpoint);
-
-export default router;
--- desafio_09/src/router/mockingproducts.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getMockingproducts } from "../controllers/mockingproducts.js";
-
-const router = express.Router();
-
-router.get("/", getMockingproducts);
-
-export default router;
